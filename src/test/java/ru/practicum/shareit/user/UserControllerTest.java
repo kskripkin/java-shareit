97740ee416ace5e4b2a424043b9c62004e7efe9c
Kirill <k.skripkin@internet.ru>
2023-02-12 14:24:46 +0300

    Еще тесты на контроллер пользователя

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -13,11 +13,14 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.practicum.shareit.user.model.User;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,32 +39,59 @@ class UserControllerTest {
     private MockMvc mvc;
 
     private User user;
+    private User updateUser;
+    private List<User> userList;
 
     @BeforeEach
     void setUp() {
         mvc = MockMvcBuilders.standaloneSetup(controller).build();
+
         user = new User();
         user.setId(1L);
         user.setName("Anna");
         user.setEmail("anna@mail.ru");
+
+        updateUser = new User();
+        updateUser.setId(1L);
+        updateUser.setName("Anna");
+        updateUser.setEmail("anna123@mail.ru");
+
+        userList = new ArrayList<>();
+        userList.add(user);
+        userList.add(updateUser);
     }
 
     @Test
     void getUser() throws Exception {
         when(userService.getUser(1L)).thenReturn(user);
 
-        mvc.perform(get("/users").
+        mvc.perform(get("/users/1")
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk())
-                        .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
-                        .andExpect(jsonPath("$.name", is(user.getName())))
-                        .andExpect(jsonPath("$.email", is(user.getEmail())));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(user.getName())))
+                .andExpect(jsonPath("$.email").value("anna@mail.ru"))
+                .andDo(print());
     }
 
     @Test
     void getUsers() throws Exception {
+        when(userService.getUsers()).thenReturn(userList);
+
+        mvc.perform(get("/users")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", is(user.getId()), Long.class))
+                .andExpect(jsonPath("$[0].name", is(user.getName())))
+                .andExpect(jsonPath("$[0].email", is(user.getEmail())))
+                .andExpect(jsonPath("$[1].id", is(updateUser.getId()), Long.class))
+                .andExpect(jsonPath("$[1].name", is(updateUser.getName())))
+                .andExpect(jsonPath("$[1].email", is(updateUser.getEmail())))
+                .andDo(print());
     }
 
     @Test
@@ -69,9 +99,9 @@ class UserControllerTest {
         when(userService.createUser(any())).thenReturn(user);
 
         mvc.perform(post("/users").content(mapper.writeValueAsString(user))
-                .characterEncoding(StandardCharsets.UTF_8)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(user.getId()), Long.class))
                 .andExpect(jsonPath("$.name", is(user.getName())))
@@ -80,18 +110,15 @@ class UserControllerTest {
 
     @Test
     void updateUser() throws Exception  {
-        User updateUser = new User();
-        updateUser.setId(1L);
-        updateUser.setName("Anna");
-        updateUser.setEmail("anna123@mail.ru");
+
         when(userService.updateUser(1L, updateUser)).thenReturn(updateUser);
 
         mvc.perform(patch("/users/1").content(mapper.writeValueAsString(updateUser))
                         .characterEncoding(StandardCharsets.UTF_8)
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk())
-                        .andExpect(jsonPath("$.email", is(updateUser.getEmail())));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email", is(updateUser.getEmail())));
     }
 
     @Test
