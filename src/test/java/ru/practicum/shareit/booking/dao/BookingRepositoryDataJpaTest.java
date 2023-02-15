00488f72ad1db47042000ec90df2b65e70709bc6
Kirill <k.skripkin@internet.ru>
2023-02-15 22:09:56 +0300

    Покрыт еще один репозиторий

---
+++ src/test/java/ru/practicum/shareit/booking/dao/BookingRepositoryDataJpaTest.java
@@ -0,0 +1,86 @@
+package ru.practicum.shareit.booking.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingRepositoryDataJpaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByItemIdAndTime() {
+    }
+
+    @Test
+    void getByItemIdAndUserId() {
+    }
+
+    @Test
+    void getByBookerId() {
+    }
+
+    @Test
+    void getByBookerIdAndCurrentTime() {
+    }
+
+    @Test
+    void getByBookerIdAndPastTime() {
+    }
+
+    @Test
+    void getByBookerIdAndFutureTime() {
+    }
+
+    @Test
+    void getByBookerIdAndStatus() {
+    }
+
+    @Test
+    void getByOwnerId() {
+    }
+
+    @Test
+    void getByOwnerIdAndCurrentTime() {
+    }
+
+    @Test
+    void getByOwnerIdAndPastTime() {
+    }
+
+    @Test
+    void getByOwnerIdAndFutureTime() {
+    }
+
+    @Test
+    void getByOwnerIdAndStatus() {
+    }
+
+    @Test
+    void getByItemIdLast() {
+    }
+
+    @Test
+    void getByItemIdNext() {
+    }
+
+    @Test
+    void getByItemId() {
+    }
+
+    @Test
+    void testGetByItemIdLast() {
+    }
+}
